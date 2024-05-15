00d9f1aacef3fff8700484c68fa8fe06723b7081
Tom Freeman <tfj1301@gmail.com>
2024-05-15 18:23:19 +0100

    more changes

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import time
 
 # This is an example python script to test merge conflicts
-time.sleep(3)
+time.sleep(4)
 print(f"Hello World!, the time is {time.time()}")
 time.sleep(3)
+
+#new lines of code here
